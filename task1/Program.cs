e37971f85f885d1b8308e6d9d150d4488fa55578
Nick <nickyyy1992@gmail.com>
2024-02-19 15:07:38 +0400

    Добавили переменные

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Console.WriteLine("Здарова Мир");
+
+int name = 100;
+Console.WriteLine(name);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c;
+Console.WriteLine(b);
